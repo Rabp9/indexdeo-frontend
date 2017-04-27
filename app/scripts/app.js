87ce9fc30f6521e4a356dd46e6383b2c4abbdbc3
Roberto André Bocanegra Palacios <rabp_91@hotmail.com>
2017-04-27 00:13:47 -0500

    Primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name inexdeoFrotendApp
+ * @description
+ * # inexdeoFrotendApp
+ *
+ * Main module of the application.
+ */
+angular
+.module('inexdeoFrotendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inexdeoFrotendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inexdeoFrotendApp
+ */
+angular.module('inexdeoFrotendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
